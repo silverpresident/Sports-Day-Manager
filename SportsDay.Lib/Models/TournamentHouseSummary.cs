2156e7d13b50c8c63837a50a2c3f8998b86f176a
silverpresident <4566006+silverpresident@users.noreply.github.com>
2025-01-12 17:56:21 +0000

    public ui presentation is good

--- SportsDay.Lib/Models/TournamentHouseSummary.cs
+++ SportsDay.Lib/Models/TournamentHouseSummary.cs
@@ -5,16 +5,29 @@ namespace SportsDay.Lib.Models
 {
     public class TournamentHouseSummary
     {
+        public TournamentHouseSummary(Tournament tournament, House house, string ageGroup, string gender)
+        {
+            Tournament = tournament ?? throw new ArgumentNullException(nameof(tournament));
+            House = house ?? throw new ArgumentNullException(nameof(house));
+            AgeGroup = ageGroup ?? throw new ArgumentNullException(nameof(ageGroup));
+            Gender = gender ?? throw new ArgumentNullException(nameof(gender));
+            
+            TournamentId = tournament.Id;
+            HouseId = house.Id;
+        }
+
+        protected TournamentHouseSummary() { } // For EF Core
+
         public Guid Id { get; set; }
 
         public Guid TournamentId { get; set; }
-        public Tournament Tournament { get; set; }
+        public Tournament? Tournament { get; set; }
 
         public int HouseId { get; set; }
-        public House House { get; set; }
+        public House? House { get; set; }
 
-        public string AgeGroup { get; set; }
-        public string Gender { get; set; }
+        public string? AgeGroup { get; set; }
+        public string? Gender { get; set; }
 
         public int Points { get; set; }
     }
